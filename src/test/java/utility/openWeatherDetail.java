e3f8e3cdb5b584f14346e376d045e4c4275cd5e9
Prasanna Pujar <prasanna4jk@gmail.com>
2020-07-05 17:20:17 +0530

    implemented java class to extract the response from openweather API

---
+++ src/test/java/utility/openWeatherDetail.java
@@ -0,0 +1,50 @@
+package utility;
+
+public class openWeatherDetail {
+
+    private String city;
+    private String condition;
+    private float windSpeed;
+    private int humidity;
+    private int tempInDegree;
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public void setWindSpeed(float windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public void setTempInDegree(int tempInDegree) {
+        this.tempInDegree = tempInDegree;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public float getWindSpeed() {
+        return windSpeed;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public int getTempInDegree() {
+        return tempInDegree;
+    }
+}
